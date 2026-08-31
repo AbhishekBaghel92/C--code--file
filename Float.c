1a192cee05997c895f4028c04fb6749a40946a53
Abhishek Baghel <abhipal92013@gmail.com>
2026-08-31 18:04:08 +0530

    Add files via upload

---
+++ Float.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float a=10.5,b=20.5;
+    float sum=a+b;
+    
+    printf("Sum: %.2f\n",sum);
+    return 0;
+}
---
+++ char.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char a='A',b='B';
+    char sum=a+b;
+    
+    printf("Sum: %c\n",sum);
+    return 0;   
+}

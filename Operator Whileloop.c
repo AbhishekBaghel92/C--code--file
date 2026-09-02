e868eeaee9d7cc98fc0d316e19740134b0ae07c8
Abhishek Baghel <abhipal92013@gmail.com>
2026-09-02 18:53:14 +0530

    Add files via upload

---
+++ Operator Whileloop.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+int main()
+// {
+//     int x, y;
+//     // x=1;
+//     // y=1;
+//     printf("Enter Start : x : number: ");
+//     scanf("%d", &x);
+//     printf("Enter End : y : number: ");
+//     scanf("%d", &y);
+//     while (x <= 5)
+//     {
+//         printf("%d\n", y);
+//         y = y + x;
+//         x++;
+//     }
+//     return 0;
+// }
+
+// {
+//     int x, y;
+    
+//     printf("Enter Start : x : number: ");
+//     scanf("%d", &x);
+//     printf("Enter End : y : number: ");
+//     scanf("%d", &y);
+//     while (x <= 5)
+//     {
+//         printf("%d\n", y);
+//         y = y *x; // we are imliment  + , - , /  , % ;
+//         x++;
+//     }
+//     return 0;
+// }
+
+
+{
+    int x, y ,z,f;
+   
+    printf("Enter Start : x : number: ");
+    scanf("%d", &x);
+    printf("Enter End : y : number: ");
+    scanf("%d", &y);
+    while (x <= 5)
+    {
+        printf("%d\n", y);
+        z = y + x;
+        x = f;
+        f= y;
+        x++;
+    }
+    return 0;
+}
---
+++ PrintNumber.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf("Enter Start : x : number: ");
+    scanf("%d", &x);
+
+    while (x <= 10)
+    {
+        printf("%d\n", x);
+        x++;
+    }
+    return 0;
+}
+
+
+
